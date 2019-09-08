518661facddd919f7f21d806bfd385c0065c5875
Eric035 <ericleung1997@gmail.com>
2019-09-07 22:46:31 -0400

    Added Pipe class

--- game.py
+++ game.py
@@ -90,10 +90,51 @@ class Bird:
     def get_mask(self):         # Class for handling collisions
         return pygame.mask.from_surface(self.motion)
 
-
 class Pipe:
-    pass
+    GAP = 200 
+    VELOCITY = 5
 
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.gap = 100
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)    # Flip pipe image upside down
+        self.PIPE_BOTTOM = PIPE_IMG
+
+        self.passed = False
+        self.set_height()
+    
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.VELOCITY
+
+    def draw(self, canvas):
+        canvas.blit(self.PIPE_TOP, (self.x, self.top))
+        canvas.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+    
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+        
+        # Offsets for collision
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        top_point = bird_mask.overlap(top_mask, top_offset)
+        bottom_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        
+        hasCollied = True if (top_point or bottom_point) else False
+        return hasCollied
+    
+class Ground:
+    pass
 
 # Main functions of game script 
 def draw_canvas(canvas, bird):
@@ -119,5 +160,5 @@ def main():
     pygame.quit()
     quit()
 
-
-main()
+if __name__ == "__main__":
+    main()
